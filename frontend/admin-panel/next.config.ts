73c992795f1c0a17dde5d02566da6bbfb59b41dc
Abdihakim <abdihakin18@gmail.com>
2025-06-25 14:51:38 +0300

    chore: Update Docker setup and admin panel configurations
    - Modified  to update service configurations
    - Added  for admin panel to exclude unnecessary files
    - Updated  to reflect new project settings in frontend/admin-panel
    - Refined  for improved build process in admin panel
    - Adjusted  for better performance and compatibility in admin panel

--- frontend/admin-panel/next.config.ts
+++ frontend/admin-panel/next.config.ts
@@ -1,14 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
+const nextConfig = {
   images: {
-    unoptimized: true, // disables incompatible image optimization
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME!,
-        pathname: process.env.NEXT_PUBLIC_IMAGE_PATHNAME!,
+        hostname: "xaqsoor.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
       },
     ],
   },
